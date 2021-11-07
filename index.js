eae9c16121b04406dffd368513e41e65f6cee79f
LOCO185 <jloco151@gmail.com>
2021-11-07 14:01:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+/*
+--Delete the wrong answers--
+1. Which of the following is/are strings?
+c) '4'
+
+2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+d) -4
+
+3. Which of the following is/are booleans?
+a) true
+b) false
+
+4. What is the result of 80 + 71.2?
+a) 151.2
+
+5. What is the result of "80" + 71.2?
+c) "8071.2"
+
+6. Does 100 + 30 produce a number or a string?
+a) number
+
+7. Does "100" + 30 produce a number or a string?
+b)string
+*/
+
+//------Submit your answers under the question------//
+
+//create three different ways to declare variables
+// let
+// var
+// const
+
+//declare a variable and reassign a value to it
+// let num = 10;
+// num = num +1;
+
+//create a variable and after that assign a string with its value being: He's got it!
+// let myStr = 'str';
+// `he's got it, ${myStr}`;
+/*
+
+1. create a variable and assign a value on how much a restaurant bill is.
+// let bill = 1000;
+// 1resturant bill is ${bill}`
+
+2. create a variable and assign a value on how much tax they should pay.
+// let tax = 'the tax is';
+// `${tax} is ${2}` ;
+
+3. create a variable that will calculate the bill * tax and its output would be: Your total bill is <total bill> $.
+// let bill= 500;
+// let tax = 400;
+// let sum = bill * tax;
+// console.log('Your total bill is' ${sum} '$')
+ */
+
+// Round the number 50.6 to its nearest integer
+// let x = Math.round(50.6)
+
+//Create a variable that is undefined
+// let firstName;
+// console.log(firstName);//undefined
